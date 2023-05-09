c322c8db71d6ad2a5dc6f9e624fdd5bc718085b7
anatawa12 <anatawa12@icloud.com>
2023-05-09 19:18:04 +0900

    generalize building src json

--- build.sh
+++ build.sh
@@ -3,6 +3,11 @@ set -eu
 
 mkdir dist
 
-jq -c . < src/vpm.src.json > dist/vpm.json
+for src_json in src/*.src.json; do
+  src_json="${src_json%.src.json}"
+  src_json="${src_json#src/}"
+  echo "$src_json"
+  jq -c . < "src/$src_json.src.json" > "dist/$src_json.json"
+done
 
 cp src/* dist
