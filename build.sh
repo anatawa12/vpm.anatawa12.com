a2135f07cc515e8995126863919a5be67f3a80c3
anatawa12 <anatawa12@icloud.com>
2023-05-09 11:55:49 +0900

    move source files to src dir

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -eu
 
+pushd src
 if test -f vpm.src.json; then
   jq -c . < vpm.src.json > vpm.json
 fi
+popd
 
 mkdir dist
-cp README.md _redirects add-repo.html vpm.src.json vpm.json dist
+cp src/* dist
