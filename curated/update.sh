fc5097d0b5f1f53da2029fd2078c12e1adb52ea5
anatawa12 <anatawa12@icloud.com>
2023-05-09 19:50:42 +0900

    fix: error if no changes

--- curated/update.sh
+++ curated/update.sh
@@ -15,6 +15,7 @@ git add ../src/curated-experimental.src.json
 
 if git diff-index --quiet --cached HEAD --; then
   echo "nothing updated!"
+  exit 0
 fi
 
 git commit -m "update curated repository"
