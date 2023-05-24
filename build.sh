2ecd053f784c53a8c76ce2173c2abf28d2178044
anatawa12 <anatawa12@icloud.com>
2023-05-24 20:07:55 +0900

    strip curated_extra

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ for src_json in src/*.src.json; do
   src_json="${src_json%.src.json}"
   src_json="${src_json#src/}"
   echo "$src_json"
-  jq -c . < "src/$src_json.src.json" > "dist/$src_json.json"
+  jq -c 'walk(if type == "object" then del(.curated_extra) else . end)' < "src/$src_json.src.json" > "dist/$src_json.json"
 done
 
 cp -r static/* dist
