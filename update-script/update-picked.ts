3ce2f4bd214832e43224d55601309ee4422f6527
anatawa12 <anatawa12@icloud.com>
2023-09-18 00:15:33 +0900

    chore: add support for range system

--- update-script/update-picked.ts
+++ update-script/update-picked.ts
@@ -1,6 +1,6 @@
 /// The script to copy some package from other repository.
 
-import * as semver from "https://deno.land/std@0.200.0/semver/mod.ts";
+import {parse as parseSemver, parseRange, SemVerRange, testRange} from "https://deno.land/std@0.200.0/semver/mod.ts";
 import {RepoUpdater} from "./update-repo.ts";
 import {RepositoryJson} from "./type.ts";
 
@@ -9,9 +9,11 @@ import {RepositoryJson} from "./type.ts";
 const repositoryPath: string = Deno.env.get("REPOSITORY_PATH") ?? throws(new Error("REPOSITORY_PATH not specified"));
 const userAgent: string = Deno.env.get("HTTP_USER_AGENT") || "vpm.anatawa12.com updater (https://github.com/anatawa12/vpm.anatawa12.com)";
 
-const config: Record<string, { pkg: string, prerelease?: boolean }[]> = {
+type PackageConfig = { pkg: string, range?: SemVerRange, prerelease?: boolean };
+
+const config: Record<string, PackageConfig[]> = {
   "https://vpm.nadena.dev/vpm-prerelease.json": [
-    {pkg: "nadena.dev.ndmf"},
+    {pkg: "nadena.dev.ndmf", range: parseRange(">= 0.3.0")},
   ],
 };
 
@@ -20,11 +22,11 @@ for (const [removePath, packages] of Object.entries(config)) {
   const remoteRepo = await fetch(removePath, {headers: {"User-Agent": userAgent}}).then(x => x.json()) as RepositoryJson;
 
   for (const pkgInfo of packages) {
-    const {pkg: packageId, prerelease} = pkgInfo;
+    const {pkg: packageId} = pkgInfo;
     const packageInfo = remoteRepo.packages[packageId];
     if (!packageInfo) throw new Error(`${packageId} not found`);
     for (const [version, packageJson] of Object.entries(packageInfo.versions)) {
-      if (prerelease || !semver.parse(version).prerelease.length) {
+      if (includeVersion(version, pkgInfo)) {
         if (!repoJson.contains(packageId, version)) {
           repoJson.addPackage(packageJson);
         }
@@ -37,6 +39,20 @@ for (const [removePath, packages] of Object.entries(config)) {
 
 repoJson.save();
 
-function throws(e: unknown): never{
+function throws(e: unknown): never {
   throw e;
+}
+
+function includeVersion(version: string, config: PackageConfig): boolean {
+  const parsedVersion = parseSemver(version);
+  const versionPrerelease = !parsedVersion.prerelease.length;
+
+  // it's prerelease and not specified as included
+  if (versionPrerelease && !config.prerelease) return false;
+
+  if (config.range) {
+    if (!testRange(parsedVersion, config.range)) return false;
+  }
+
+  return true;
 }
