3d1abe89f51b3fe3720136c9a9142135e5503e07
anatawa12 <anatawa12@icloud.com>
2024-06-28 15:59:12 +0900

    feat: Thrrotle connections

--- update-script/set-zipSHA256.ts
+++ update-script/set-zipSHA256.ts
@@ -8,13 +8,47 @@ const promises: Promise<void>[] = [];
 
 const userAgent = "vpm.anatawa12.com updater (https://github.com/anatawa12/vpm.anatawa12.com)";
 
-for (const value of Object.values(repo.packages)) {
+class Thrrotle {
+  #maxCount: number;
+  #runningCount: number = 0;
+  #waiting: (() => void)[] = [];
+
+  constructor(maxCount: number) {
+    this.#maxCount = maxCount;
+  }
+
+  async run<T>(fn: () => Promise<T>): Promise<T> {
+    if (this.#runningCount >= this.#maxCount) {
+      await new Promise<void>((resolve) => this.#waiting.push(resolve));
+    }
+
+    this.#runningCount++;
+    try {
+      return await fn();
+    } finally {
+      this.#runningCount--;
+      if (this.#waiting.length > 0) {
+        this.#waiting.shift()!();
+      }
+    }
+  }
+}
+
+const throttle = new Thrrotle(10);
+
+for (const [pkgId, value] of Object.entries(repo.packages)) {
+  if (!pkgId.startsWith("com.anatawa12.")) {
+    console.log(`Skipping ${pkgId}`)
+    continue
+  }
   for (const version of Object.values(value.versions)) {
     if (!version.zipSHA256 && version.url) {
       const url = version.url;
-      promises.push((async () => {
+      promises.push(throttle.run(async () => {
         try {
-          console.log(`Downloading ${url} (${version.name} v${version.version})...`)
+          const shortId = version.name.substring("com.anatawa12.".length);
+          console.log(`Downloading ${shortId} v${version.version} ${url}...`)
+
           const zipData = await fetch(url, {headers: {"User-Agent": userAgent}})
             .then(res => {
               if (!res.ok) throw new Error(`Downloading file failed: ${url}`);
@@ -27,14 +61,14 @@ for (const value of Object.values(repo.packages)) {
             .map((b) => b.toString(16).padStart(2, "0"))
             .join("")
 
-          console.log(`Downloaded ${url} with SHA256: ${sha256DigitHex}`)
+          console.log(`Downloaded ${shortId} v${version.version} with SHA256: ${sha256DigitHex} size: ${zipData.byteLength} bytes`)
 
           version.zipSHA256 = sha256DigitHex;
         } catch (e) {
           console.error(`Failed to download ${url}: ${e}`)
           throw e;
         }
-      })())
+      }))
     }
   }
 }
